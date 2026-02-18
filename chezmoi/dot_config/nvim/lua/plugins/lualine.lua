0e4c454a769739fc3a889e4a9ab8fb6f3686050b
Your Name <you@example.com>
2026-02-18 19:13:26 +0100

    fix treesitter and add lualine

---
+++ chezmoi/dot_config/nvim/lua/plugins/lualine.lua
@@ -0,0 +1,113 @@
+return {
+  "nvim-lualine/lualine.nvim",
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+  opts = function()
+    local icons = {
+      diagnostics = {
+        Error = " ",
+        Warn = " ",
+        Info = " ",
+        Hint = " ",
+      },
+      git = {
+        added = " ",
+        modified = " ",
+        removed = " ",
+      },
+    }
+
+    local function pretty_path()
+      local path = vim.fn.expand("%:~:.")
+      if path == "" then
+        return ""
+      end
+
+      local filename = vim.fn.expand("%:t")
+      local extension = vim.fn.expand("%:e")
+      local icon = ""
+      local ok, devicons = pcall(require, "nvim-web-devicons")
+      if ok then
+        icon = devicons.get_icon(filename, extension, { default = true }) or icon
+      end
+
+      local max_length = 48
+      if #path > max_length then
+        path = "..." .. path:sub(-(max_length - 3))
+      end
+
+      return icon .. " " .. path
+    end
+
+    local lualine_y = {}
+    local has_codecompanion, codecompanion = pcall(require, "lualine.components.codecompanion")
+    if has_codecompanion then
+      table.insert(lualine_y, codecompanion)
+    end
+
+    vim.o.laststatus = 3
+
+    local opts = {
+      options = {
+        theme = "auto",
+        icons_enabled = true,
+        disabled_filetypes = { statusline = { "dashboard", "alpha", "ministarter", "snacks_dashboard" } },
+      },
+      sections = {
+        lualine_a = { "mode" },
+
+        lualine_b = {
+          {
+            "diagnostics",
+            symbols = {
+              error = icons.diagnostics.Error,
+              warn = icons.diagnostics.Warn,
+              info = icons.diagnostics.Info,
+              hint = icons.diagnostics.Hint,
+            },
+          },
+          { pretty_path },
+        },
+        lualine_c = {},
+        lualine_x = {
+          {
+            "diff",
+            symbols = {
+              added = icons.git.added,
+              modified = icons.git.modified,
+              removed = icons.git.removed,
+            },
+            source = function()
+              local gitsigns = vim.b.gitsigns_status_dict
+              if gitsigns then
+                return {
+                  added = gitsigns.added,
+                  modified = gitsigns.changed,
+                  removed = gitsigns.removed,
+                }
+              end
+            end,
+          },
+        },
+        lualine_y = lualine_y,
+        lualine_z = {
+          function()
+            return " " .. os.date("%R")
+          end,
+        },
+      },
+      inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = { "filename" },
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = {},
+      },
+      extensions = { "lazy" },
+    }
+
+    return opts
+  end,
+}
--- chezmoi/dot_config/nvim/lua/plugins/treesitter.lua
+++ chezmoi/dot_config/nvim/lua/plugins/treesitter.lua
@@ -1,22 +1,35 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    lazy = false,            -- README: plugin does not support lazy-loading
-    build = ":TSUpdate",     -- README: keep parsers in sync with plugin
-    config = function()
-      require("nvim-treesitter").install({
-          "go", 
-          "lua", 
-          "rust", 
-          "python", 
-          "json", 
-          "yaml", 
-          "markdown", 
-          "dockerfile", 
-          "javascript", 
-          "typescript"
-      })
-    end,
-  },
-}
+    {
+        "nvim-treesitter/nvim-treesitter",
+        lazy = false,        -- README: plugin does not support lazy-loading
+        build = ":TSUpdate", -- README: keep parsers in sync with plugin
+        config = function()
+            require("nvim-treesitter").install({
+                "go",
+                "lua",
+                "rust",
+                "python",
+                "json",
+                "yaml",
+                "markdown",
+                "dockerfile",
+                "javascript",
+                "typescript"
+            })
 
+            vim.api.nvim_create_autocmd("FileType", {
+                group = vim.api.nvim_create_augroup("tree - sitter - enable", { clear = true }),
+                callback = function(args)
+                    local lang = vim.treesitter.language.get_lang(args.match)
+                    if not lang then return end
+
+                    if vim.treesitter.query.get(lang, "highlights") then vim.treesitter.start(args.buf) end
+
+                    if vim.treesitter.query.get(lang, "indents") then
+                        vim.opt_local.indentexpr = 'v:lua.require(“nvim-treesitter”).indentexpr()'
+                    end
+               end,
+            })
+        end,
+    }
+}
